80d0c75051648a2d9b4fa3371e8a847d7ef5eb15
Junayed Noman <junayednoman05@gmail.com>
2024-11-02 20:40:34 +0600

    feat:  Sorting Objects problem solved

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // 1.Task: Array Filtering and Mapping
+// Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result.
 const people = [
   { name: "John", age: 22, gender: "male" },
   { name: "Sarah", age: 25, gender: "female" },
@@ -16,6 +17,7 @@ const filterRemainingPeople = (arrayOfPeople) => {
 filterRemainingPeople(people);
 
 // 2.Task: Object Manipulation
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
 const books = [
   { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
   { title: "1984", author: "George Orwell", year: 1949 },
@@ -23,10 +25,52 @@ const books = [
   { title: "Moby Dick", author: "Herman Melville", year: 1851 },
 ];
 
-const mapBookTitles = (books) => {
-  const bookTitles = books.map((book) => book.title);
-  return bookTitles;
+const mapBookTitles = (arr) => {
+  const titles = arr.map((item) => item.title);
+  return titles;
 };
 
 const bookTitles = mapBookTitles(books);
 console.log(bookTitles);
+
+//  3.Task: Function Composition
+//  Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
+
+const squareNumber = (num) => {
+  return num * num;
+};
+
+const doubleNumber = (num) => {
+  return num * 2;
+};
+const addFive = (num) => {
+  return num + 5;
+};
+
+const squareDoubleNumberAndAddFive = (num) => {
+  const squaredNumber = squareNumber(num);
+  const doubledNumber = doubleNumber(squaredNumber);
+  const fiveAddedNumber = addFive(doubledNumber);
+  return fiveAddedNumber;
+};
+
+const finalCalculatedNumber = squareDoubleNumberAndAddFive(5);
+console.log(finalCalculatedNumber);
+
+// 4.Task: Sorting Objects
+// Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+
+const cars = [
+  { make: "Toyota", model: "Camry", year: 2021 },
+  { make: "Honda", model: "Civic", year: 2020 },
+  { make: "Ford", model: "Mustang", year: 2022 },
+  { make: "Chevrolet", model: "Malibu", year: 2019 },
+  { make: "Tesla", model: "Model 3", year: 2023 },
+];
+
+const sortAscending = (arr) => {
+  const sortedArr = arr.sort((a, b) => a.year - b.year);
+  console.log(sortedArr);
+};
+
+sortAscending(cars);
